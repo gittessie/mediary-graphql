71a34edfd05dbc2e7053b380f3d18822436a1cc8
Tessie Urtubia <gittessie@users.noreply.github.com>
2020-07-12 13:18:47 -0400

    add authentication with server

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if(req.method === 'OPTIONS'){
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(checkAuth);
 
 app.use(
@@ -27,8 +37,8 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.hm6aq.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(3000, () => {
-      console.log("Listening on port 3000");
+    app.listen(8000, () => {
+      console.log("Listening on port 8000");
     });
   })
   .catch((e) => {
